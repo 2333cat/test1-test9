d583a25dac0f0dbab6a922bf43041fc75b33979e
2333cat <113259795@qq.com>
2017-06-08 19:14:32 +0800

    first commit

--- final project/src/ADC.h
+++ final project/src/ADC.h
@@ -4,5 +4,5 @@
 void ADC_Init(void);
 void delay(uint32_t ulTime);
 void ADC_read(void );
-float   CaculTwoPoint(float   x1, float y1, float x2, float y2, float x);
+float CaculTwoPoint(float   x1, float y1, float x2, float y2, float x);
 #endif
